cead9cca146eaa61d66532eda6baed344630fbd6
U-DreamPC\Thomas <thomas.laurent38@gmail.com>
2020-03-31 21:06:17 +0200

    Last version

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test{
     public static void main(String[] args){
         String url = "jdbc:postgresql:projetBD";
         String username = "postgres";
-        String pass = "mine1405";
+        String pass = "teo";
         try{
             Class.forName("org.postgresql.Driver");
         }catch (java.lang.ClassNotFoundException e) {
